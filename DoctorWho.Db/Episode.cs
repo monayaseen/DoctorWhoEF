2a731dbe2c288b9f51eb9f70aecc72c937330981
muna yassin <muna.yassin@gmail.com>
2021-09-05 18:37:23 +0300

    Create the data models

---
+++ DoctorWho.Db/Episode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DoctorWho.Db
+{
+    public class Episode
+    {
+        public Episode()
+        {
+            EpisodeEnemyies = new List<EpisodeEnemy>();
+            EpisodeCompanions = new List<EpisodeCompanion>();
+        }
+
+        public int EpisodeId { get; set; }
+        public int SeriesNumber { get; set; }
+        public int EpisodeNumber { get; set; }
+        public string EpisodeType { get; set; }
+        public string Title { get; set; }
+        public DateTime EpisodeDate { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        public string Notes { get; set; }
+        public List<EpisodeEnemy> EpisodeEnemyies { get; set; }
+        public List<EpisodeCompanion> EpisodeCompanions { get; set; }
+    }
+}
---
+++ DoctorWho.Db/EpisodeCompanion.cs
@@ -0,0 +1,10 @@
+﻿namespace DoctorWho.Db
+{
+    public class EpisodeCompanion
+    {
+        public int EpisodeId { get; set; }
+        public Episode Episode { get; set; }
+        public int CompanionId { get; set; }
+        public Companion Companion { get; set; }
+    }
+}
